1939743b08fb82e3dcc8f2fe847a35efde2df6bf
Edgar Karpowicz <edgarkarpowicz@live.com>
2024-04-09 23:32:56 -0300

    Delete Clase 5/Ejercicio 16/EJ16 directory

--- Clase 5/Ejercicio 16/EJ16/modelos.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Optional
-
-class Usuario(BaseModel):
-    id: Optional[str] = Field(default_factory=str, alias="_id")
-    nombre: str
-    apellido: str
-    edad: int = Field(gt=0)
-    email: str
